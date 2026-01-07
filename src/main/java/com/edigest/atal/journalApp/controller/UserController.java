f8a7715b5918ec170ecab33a6dc677285b9b05e3
Atal Sohail <atal.sohail@ebitlogix.com>
2026-01-07 13:34:49 +0500

    some basic programs for better understanding of JAVA

--- src/main/java/com/edigest/atal/journalApp/controller/UserController.java
+++ src/main/java/com/edigest/atal/journalApp/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -50,16 +51,133 @@ public class UserController {
 
     @GetMapping("/greeting")
     public ResponseEntity<?> greeting() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        WeatherResponse weatherResponse = weatherService.getWeather("Lahore");
-        String greeting = "";
-        if (weatherResponse != null) {
-            greeting = ", Weather feels like " + weatherResponse.getCurrent().getFeelsLike();
-        }
+        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // WeatherResponse weatherResponse = weatherService.getWeather("Lahore");
+        // String greeting = "";
+        // if (weatherResponse != null) {
+        //     greeting = ", Weather feels like " + weatherResponse.getCurrent().getFeelsLike();
+        // }
 
         
-        return  new ResponseEntity<>("Hi " + authentication.getName() + greeting, HttpStatus.OK);
-        // return  new ResponseEntity<>("Hi " + this.threeDivisiors(8) + " " + this.threeDivisiors(25), HttpStatus.OK);
+        // return  new ResponseEntity<>("Hi " + authentication.getName() + greeting, HttpStatus.OK);
+        return  new ResponseEntity<>("Hi " + this.playWithStrings() + " ", HttpStatus.OK);
+    }
+
+    //programming tests
+    private List<String> thridEle() {
+        String[] elements = {"Hi", "Hello", "How", "Atal", "Sohail", "Huzaifa", "Ali", "Bit", "Logix"};
+
+        List<String> finalElements = new ArrayList<>();
+        int j = 0;
+        for (int i=0; i<elements.length; i++) {
+            j++;
+            if (j == 3) {
+                finalElements.add(elements[i]);
+                j = 0;
+            }
+        }
+
+        return finalElements;
+    }
+
+    private List<Integer> prime() {
+        int[] someNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+        List<Integer> primeNumbers = new ArrayList<>();
+
+        for (int number: someNumbers) {
+            int j=0;
+            for (int i=1; i<number; i++) {
+                if (number % i == 0) {
+                    j++;
+                }
+            }
+            if (j == 1) {
+                primeNumbers.add(number);
+            }
+        }
+        return primeNumbers;
+    }
+
+    private List<Integer> difference() {
+        int[] numbersA = {1,2,3,4,5,6};
+        int[] numbersB = {2,4,6,8,10};
+
+        List<Integer> differenceNumber = new ArrayList<>();
+        for (int secondNum: numbersB) {
+            boolean found = false;
+            for (int firstNum: numbersA) {
+                if (secondNum == firstNum) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false) {
+                differenceNumber.add(secondNum);
+            }
+        }
+        return differenceNumber;
+    }
+
+    private int fibonaci(int num) {
+        int a = 0;
+        int b = 1;
+        int fib = 0;
+        if (num == 1) {
+            return a;
+        }
+        if (num == 2) {
+            return b;
+        }
+        for (int i=3; i<num; i++) {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+
+        return a + b;
+    }
+
+    private List<String> nonRepeating() {
+        String[] names = {"Atal", "Sohail", "atal", "sohail"};
+        List<String> uniqueNames = new ArrayList<>();
+        
+        for (String name : names) {
+            boolean found = false;
+
+            for (String unqiueNames : uniqueNames) {
+                if (unqiueNames.equalsIgnoreCase(name)) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found) {
+                uniqueNames.add(name);
+            }
+        }
+
+        return uniqueNames;
+    }
+
+    private boolean threeDivisiors(int num) {
+        int count = 0;
+
+        for (int i=1; i<=num; i++)  {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        return count == 3;
+    }
+
+    private List<String> playWithStrings() {
+        String[] fruits = {"Apple", "Orange", "Banana"};
+        for (int i=0; i<fruits.length; i++) {
+            String fruit = fruits[i];
+            fruit = fruit + "s";
+        }
+        return Arrays.asList(fruits);
     }
 
 }
