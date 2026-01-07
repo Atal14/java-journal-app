4f93181c4a5c3485a919a43b6536290ecda9ddb7
Atal Sohail <atal.sohail@ebitlogix.com>
2026-01-07 15:44:06 +0500

    using post construct to get config data from backend.

--- src/main/java/com/edigest/atal/journalApp/controller/Admin.java
+++ src/main/java/com/edigest/atal/journalApp/controller/Admin.java
@@ -1,5 +1,6 @@
 package com.edigest.atal.journalApp.controller;
 
+import com.edigest.atal.journalApp.cache.AppCache;
 import com.edigest.atal.journalApp.entity.User;
 import com.edigest.atal.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ public class Admin {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AppCache appCache;
+
     @GetMapping("all-users")
     public ResponseEntity<?> getAllUsers() {
         List<User> users = userService.getAll();
@@ -29,5 +33,11 @@ public class Admin {
     public ResponseEntity<?> createAdminUser(@RequestBody User user) {
         User createdUser = userService.createAdminUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("clear-app-cache")
+    public ResponseEntity<?> clearAppCache() {
+        appCache.init();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/edigest/atal/journalApp/service/WeatherService.java
+++ src/main/java/com/edigest/atal/journalApp/service/WeatherService.java
@@ -1,26 +1,29 @@
 package com.edigest.atal.journalApp.service;
 
 import com.edigest.atal.journalApp.api.response.WeatherResponse;
+import com.edigest.atal.journalApp.cache.AppCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
     @Value("${weather.api.key}")
     private String apiKey;
 
-    private static final String fullUrl = "http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=CITY";
-
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private AppCache appCache;
+
     public WeatherResponse getWeather(String city) {
-        String finalApi = fullUrl.replace("YOUR_ACCESS_KEY", apiKey).replace("CITY", city);
+        String finalApi = appCache.appCache.get("WEATHER_API").replace("<apiKey>", apiKey).replace("<city>", city);
         ResponseEntity<WeatherResponse> response = this.restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
         return response.getBody();
     }
