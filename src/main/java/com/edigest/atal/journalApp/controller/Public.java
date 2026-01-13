d49d596e9034427dff6f7b32e4e42347457909d0
Atal Sohail <atal.sohail@ebitlogix.com>
2026-01-13 19:21:57 +0500

    add JWT for autentication

--- src/main/java/com/edigest/atal/journalApp/controller/Public.java
+++ src/main/java/com/edigest/atal/journalApp/controller/Public.java
@@ -1,19 +1,59 @@
 package com.edigest.atal.journalApp.controller;
 
 import com.edigest.atal.journalApp.entity.User;
+import com.edigest.atal.journalApp.service.UserDetailsServiceImpl;
 import com.edigest.atal.journalApp.service.UserService;
+import com.edigest.atal.journalApp.utils.JwtUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class Public {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
     private UserService userService;
 
-    @PostMapping("/create-user")
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
+
+    @PostMapping("/signup")
     public User createUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occurred while createAuthenticationToken ", e);
+            return new ResponseEntity<>("Incorrect Username or Password", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
